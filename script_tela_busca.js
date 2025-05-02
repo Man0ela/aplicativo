1001315c9b686800fe5770451599646bc816616e
Man0ela <163747420+Man0ela@users.noreply.github.com>
2025-05-01 22:20:52 -0300

    Revert "Merge branch 'main' of https://github.com/Man0ela/aplicativo"
    This reverts commit 99e3a3e0ab1c825aaec6a07977dd63e05910404a, reversing
    changes made to c932e4817e36349385ce33f6b6bb5866d4fc7f90.

--- script_tela_busca.js
+++ script_tela_busca.js
@@ -1,6 +1,5 @@
 function buscarProfissionais() {
-    const tipoServico = document.getElementById("barra-pesquisa").value;
-    console.log("Buscando por:", tipoServico);
-    window.location.href = `profs_filtrados.html?tipo=${encodeURIComponent(tipoServico)}`;
+    const tipo = document.getElementById('tipo-servico').value;
+    window.location.href = `profs_filtrados.html?tipo=${encodeURIComponent(tipo)}`;
   }
   
