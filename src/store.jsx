09117f2abaf7ea3368c1192ffab89cc7bdee8781
falcao-2005 <fernandoaefm@gmail.com>
2025-06-03 14:19:07 -0300

    Update store.jsx

--- src/store.jsx
+++ src/store.jsx
@@ -2,13 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import buscaReducer from './features/buscaSlice.jsx';
 import telainicialReducer from './features/telainicialSlice';
 import profissionaisReducer from './features/profsFiltradosSlice';
-import profdetalhesReducer from './features/profDetalhesSlice'; // ajuste o path conforme sua estrutura
+import profdetalhesReducer from './features/profDetalhesSlice';
 import footerReducer from "./features/footerSlice.jsx";
 import headerReducer from "./features/headerSlice.jsx";
 import SobreNosReducer from './features/sobrenosSlice.jsx';
 import ServicosReducer from './features/servicosSlice.jsx';
 
-import dashboardProfissionalReducer from './features/dashboardProfissionalSlice.jsx'; // importe o novo slice
+import dashboardProfissionalReducer from './features/dashboardProfissionalSlice.jsx';
 
 // Configure a store com o slice
 export const store = configureStore({
@@ -22,7 +22,6 @@ export const store = configureStore({
     sobreNos: SobreNosReducer,
     servicosContratados: ServicosReducer,
 
-    dashboardProfissional: dashboardProfissionalReducer // adicione aqui
+    dashboardProfissional:
   }
 });
-
